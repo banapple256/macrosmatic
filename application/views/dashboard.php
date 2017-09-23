7463cb4e5a53daf7629b3b8744dd4756301b0a44
BILAL ZAFAR <bilalzafar256@gmail.com>
2017-09-23 22:32:49 +0500

    Added

---
+++ application/views/dashboard.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<title>Dashbaord</title>
+<div class="main-content">
+	<?php include 'template/topmenu.php' ?>
+</div>
---
+++ application/views/template/sidebar.php
@@ -0,0 +1,35 @@
+<div class="sidebar-menu">
+    <div class="sidebar-menu-inner">
+        <header class="logo-env">
+            <div class="logo">
+                <a href="javascript:;"> <img src="<?php echo base_url(); ?>assets/images/logo@2x.png" width="120" alt="logo" /> </a>
+            </div>
+            <div class="sidebar-collapse">
+                <a href="#" class="sidebar-collapse-icon with-animation">
+                    <i class="entypo-menu"></i> 
+				</a>
+            </div>
+            <div class="sidebar-mobile-menu visible-xs">
+                <a href="#" class="with-animation">
+                    <i class="entypo-menu"></i> 
+				</a>
+            </div>
+        </header>
+        <ul id="main-menu" class="main-menu">
+			<li> <a href="javascript:;" <i class="entypo-monitor"></i><span class="title"> Dashbaord</span></a> </li>
+			<li class="has-sub"> <a href="javascript:;"><i class="entypo-palette"></i><span class="title">Skins</span></a>
+                <ul>
+                    <li> <a href="javascript:;"><span class="title">Black Skin</span></a> </li>
+                    <li> <a href="javascript:;"><span class="title">White Skin</span></a> </li>
+                    <li> <a href="javascript:;"><span class="title">Purple Skin</span></a> </li>
+                    <li> <a href="javascript:;"><span class="title">Cafe Skin</span></a> </li>
+                    <li> <a href="javascript:;"><span class="title">Red Skin</span></a> </li>
+                    <li> <a href="javascript:;"><span class="title">Green Skin</span></a> </li>
+                    <li> <a href="javascript:;"><span class="title">Yellow Skin</span></a> </li>
+                    <li> <a href="javascript:;"><span class="title">Blue Skin</span></a> </li>
+                    <li> <a href="javascript:;"><span class="title">Facebook Skin</span></a> </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
